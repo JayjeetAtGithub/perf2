2767e6acc804acbbac6b5c99a137c61c0b8f22b6
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-12-13 04:55:47 +0000

    fixes

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@ set -ex
 BINARY_DIR=/usr/local/bin
 
 g++ -O3 \
-    -std=c++17 \
+    -std=c++23 \
     perf_cpu.cc \
     -fomit-frame-pointer \
     -march=native \
     -o ${BINARY_DIR}/perf_cpu
 
 g++ -O3 \
-    -std=c++17 \
+    -std=c++23 \
     perf_amx.cc \
     -ldnnl \
     -fomit-frame-pointer \
--- dist.hpp
+++ dist.hpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <immintrin.h>
 #include <unordered_map>
+#include <stdfloat>
 
 #include "oneapi/dnnl/dnnl.hpp"
 
@@ -16,6 +17,7 @@
 
 using tag = dnnl::memory::format_tag;
 using dt = dnnl::memory::data_type;
+using bf16 = std::bfloat16_t;
 
 static bool is_amxbf16_supported() {
   unsigned int eax, ebx, ecx, edx;
@@ -52,7 +54,7 @@ static void write_to_dnnl_memory(void const *handle, dnnl::memory &mem) {
 }
 
 static void amx_matmul(int32_t const &r1, int32_t const &r2, const int32_t &c,
-                       const float *a, const float *b, dnnl::engine &engine,
+                       const bf16 *a, const bf16 *b, dnnl::engine &engine,
                        dnnl::stream &stream) {
   dnnl::memory::dims a_dims = {r1, c};
   dnnl::memory::dims b_dims = {c, r2};
@@ -87,32 +89,24 @@ static void amx_matmul(int32_t const &r1, int32_t const &r2, const int32_t &c,
 }
 
 static void amx_inner_product(int32_t const &n, int32_t const &oc,
-                              int32_t const &ic, const float *s, const float *w,
+                              int32_t const &ic, const bf16 *s, const bf16 *w,
                               dnnl::engine &engine, dnnl::stream &stream) {
 
   dnnl::memory::dims s_dims = {n, ic};
   dnnl::memory::dims w_dims = {oc, ic};
   dnnl::memory::dims dst_dims = {n, oc};
 
-  auto s_in_md = dnnl::memory::desc(s_dims, dt::f32, tag::ab);
-  auto w_in_md = dnnl::memory::desc(w_dims, dt::f32, tag::ab);
-  auto dst_out_md = dnnl::memory::desc(dst_dims, dt::f32, tag::ab);
-  auto s_in_mem = dnnl::memory(s_in_md, engine);
-  auto w_in_mem = dnnl::memory(w_in_md, engine);
-  write_to_dnnl_memory(s, s_in_mem);
-  write_to_dnnl_memory(w, w_in_mem);
-
-  auto s_md = dnnl::memory::desc(s_dims, dt::bf16, tag::any);
-  auto w_md = dnnl::memory::desc(w_dims, dt::bf16, tag::any);
-  auto dst_md = dnnl::memory::desc(dst_dims, dt::bf16, tag::any);
+  auto s_md = dnnl::memory::desc(s_dims, dt::bf16, tag::ab);
+  auto w_md = dnnl::memory::desc(w_dims, dt::bf16, tag::ab);
+  auto dst_md = dnnl::memory::desc(dst_dims, dt::bf16, tag::ab);
+  
+  auto s_mem = dnnl::memory(s_md, engine);
+  auto w_mem = dnnl::memory(w_md, engine);
+  write_to_dnnl_memory(s, s_mem);
+  write_to_dnnl_memory(w, w_mem);
 
   auto pd = dnnl::inner_product_forward::primitive_desc(
       engine, dnnl::prop_kind::forward_training, s_md, w_md, dst_md);
-
-  auto s_mem = dnnl::memory(pd.src_desc(), engine);
-  dnnl::reorder(s_in_mem, s_mem).execute(stream, s_in_mem, s_mem);
-  auto w_mem = dnnl::memory(pd.weights_desc(), engine);
-  dnnl::reorder(w_in_mem, w_mem).execute(stream, w_in_mem, w_mem);
   auto dst_mem = dnnl::memory(pd.dst_desc(), engine);
 
   auto prim = dnnl::inner_product_forward(pd);
