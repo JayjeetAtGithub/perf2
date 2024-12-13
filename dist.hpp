bf6c29efa06c6cf56bb4720244a3ce1e17e2947c
Jayjeet Chakraborty <jc.github@rediffmail.com>
2024-12-13 05:19:56 +0000

    Fixes

--- dist.hpp
+++ dist.hpp
@@ -80,7 +80,7 @@ static void amx_matmul(int32_t const &r1, int32_t const &r2, const int32_t &c,
   stream.wait();
 }
 
-static void amx_inner_product(int32_t const &n, int32_t const &oc,
+static auto amx_inner_product(int32_t const &n, int32_t const &oc,
                               int32_t const &ic, const bf16 *s, const bf16 *w,
                               dnnl::engine &engine, dnnl::stream &stream) {
 
@@ -106,8 +106,12 @@ static void amx_inner_product(int32_t const &n, int32_t const &oc,
   args.insert({DNNL_ARG_SRC, s_mem});
   args.insert({DNNL_ARG_WEIGHTS, w_mem});
   args.insert({DNNL_ARG_DST, dst_mem});
+
+  auto s = std::chrono::high_resolution_clock::now();
   prim.execute(stream, args);
   stream.wait();
+  auto e = std::chrono::high_resolution_clock::now();
+  return std::chrono::duration_cast<std::chrono::microseconds>(e - s).count();
 }
 
 static float inner_product(void const *vec1, void const *vec2,
--- perf_amx.cc
+++ perf_amx.cc
@@ -60,12 +60,8 @@ public:
     std::string dims =
         std::to_string(N1) + "/" + std::to_string(N2) + "/" + std::to_string(M);
     {
-      auto start = std::chrono::high_resolution_clock::now();
-      amx_inner_product(N1, N2, M, mat_a.data(), mat_b.data(), engine, stream);
-      auto end = std::chrono::high_resolution_clock::now();
-      auto dur =
-          std::chrono::duration_cast<std::chrono::microseconds>(end - start)
-              .count();
+      auto dur = amx_inner_product(
+        N1, N2, M, mat_a.data(), mat_b.data(), engine, stream);
       double gflops =
           ((double)(total_flop / BILLION)) / ((double)(dur / MILLION));
       pt->addRow("IP / AMX", dims, data_size, total_flop, dur, gflops);
